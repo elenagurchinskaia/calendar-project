5e0edd55f95aa6ba706b2a075f17049230eebbca
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-08 17:29:32 -0500

    added starter code

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # calendar-project
+
+## Description
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours of 9am to 5pm
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## Use
+
+- Go to the website.
+- Click on "Generate Password" button.
+- Select needed length of the characters.
+- Select or opt out of the special characters provided.
+- Password is generated.
+- Enjoy!
+
+## Technology Used
+
+[JavaScript](!--https://developer.mozilla.org/en-US/docs/Web/JavaScript--)
+[Day.js](!--https://day.js.org--)
+[HTML](!--https://developer.mozilla.org/en-US/docs/Web/HTML--)
+[CSS](!--https://developer.mozilla.org/en-US/docs/Web/CSS--)
+[Git](!--https://git-scm.com--)
+
+## Learning points
+
+- Using JavaScript "Prompt", "Confirm" and "Alert".
+- Using "Math.randon" to randomize the password.
+- Using "generateBtn".
+
+## Deployment
+
+[Password Generator](https://elenagurchinskaia.github.io/password-generator/)
+
+![alt text](./assets/images/05-third-party-apis-homework-demo.gif)
+
+## Author
+
+### Elena Gurchinskaia
+
+- [LinkedIn](https://www.linkedin.com/in/elena-gurchinskaia-4969ab104/)
+- [Github](https://github.com/elenagurchinskaia/)
+
+## License
+
+N/A
---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+// use local storage to store the data
+$(function () {
+  // TODO:Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
+  // a. $(".saveBTn").on ('click', function() {
+  // console.log($(this));
+  // figure out what hour block you're in by using $(this)
+  // key needs to match the key names for getItem localStorage
+  // setItem for localStorage to save the input to the corresponding hour by using keyword 'this'
+  // });
+
+  // TODO:Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+
+  // a. use jquery.children to select children elements
+
+  // JS Vanilla version:
+  document.querySelectorAll(".time-block"); //return an array of all time block names and can be used for the for loop (jquery)
+
+  // b. use dayjs( to grab current time (the hour))
+  // c. use for loop for the time blocks
+  // > inside the for loop select the time block
+  for (i = 9; i < 18; i++) document.querySelector("#hour-" + i); // jquery selector
+  // > use if statement to compare the current hour to the time block
+  // > if the current hour is less than time block apply future
+  // > else if the current hour is equal to time block apply present class
+  // > else the current hour is greater than time block apply past class
+  // d. remove past, present, and future class in html and append the correct class
+  // e. can use setIterval to create auto refresh
+
+  // TODO:Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+
+  // a. getItem for localStorage to pull previously stored content
+  // > separate key in localStorage for each hour
+  // > create a for loop for hour 9-18 to check localStorage
+  //check localStorage within the loop
+  // if there is storage, display the items (value)
+  // inside the text area of corresponding hour
+  // use id selector for the text area
+
+  // TODO:Add code to display the current date in the header of the page.
+
+  // a. create var for dayjs  to get current date (and time)
+  // b. $("#currentDay").text(var)
+});
